81f2f231d44bf0ece8fee4fe9d972f137f4e8e4d
Guillaume DEVAUX (Auxioma) <auxioma.g@yahoo.com>
2025-05-01 14:49:33 +0200

    correction lang

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,13 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoryCrudController extends AbstractCrudController
 {
@@ -24,7 +25,12 @@ class CategoryCrudController extends AbstractCrudController
             IdField::new('id')->onlyOnIndex(),
             TextField::new('name'),
             SlugField::new('slug')->onlyOnForms()->setTargetFieldName('name'),
-            AssociationField::new('parent')
+            AssociationField::new('parent'),
+            ChoiceField::new('lang')
+                ->setChoices([
+                    'Français' => 'fr',
+                    'Anglais' => 'en'
+                ])
                
         ];
     }
